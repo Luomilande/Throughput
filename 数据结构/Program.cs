e247ebefd1a1000e4b258aab106ba0c33a0067a7
CoCo <merb001@outlook.com>
2020-09-06 01:13:32 +0800

    添加core项目，添加数据结构项目

---
+++ 数据结构/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 数据结构
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
